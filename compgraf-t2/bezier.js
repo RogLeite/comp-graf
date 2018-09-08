3287b963077abdd66d66178a24dfe96fca614ba1
RogLeite <rodrigo.ldasilva@hotmail.com>
2018-09-08 12:13:14 -0300

    Ajustada nas classes

--- compgraf-t2/bezier.js
+++ compgraf-t2/bezier.js
@@ -2,48 +2,23 @@ var canvas =document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var pickedPoints = [];
 
-function interpolatePoints(){
-	if (pickedPoints.length <=1){
-		//donothing
-	}
-	else if(pickedPoints.length===2){
-		//define primeiras derivadas
-		let p1 = pickedPoints[0], p2 = pickedPoints[1];
-		p1.setR(calcSlope(p1,p2),calcModule(p1,p2));
-	}
-	else {
-		//define derivadas normalmente
-	}
 
-}
-function calcSlope(p1,p2){
-	return ((p2.y-p1.y)/(p2.x-p1.x));
-}
 
 
 function pickPoint(point){
-	let fullPoint = {
-		x:point.x,
-		y:point.y,
-		l:0,
-		r:0,
-		setR : function(slope,mod){
-			this.r = R;//[[TODO]] construir pontos para usar como r e l
-		},
-		setL : function(L){
-			this.l = L;
-		}
-	};
-	pickedPoints.push(fullPoint);
-	interpolatePoints();
+	if(pickedPoints.length===1){
+		pickedPoints[0].interpolateTo(point);
+	}else if(pickedPoints.length===2){
+		pickedPoints[0].interpolateBetween(pickedPoints[1],point);
+	}else if(pickedPoints.length>2){
+		pickedPoints[pickedPoints.length-1].interpolateNew(point);
+	}
+	pickedPoints.push(point);
 }
 
 function getPoint(evt){
 	let ret = canvas.getBoundingClientRect();
-	return {
-		x: evt.clientX-ret.x,
-		y: evt.clientY-ret.y
-	};
+	return new class_fullPoint(evt.clientX-ret.x,evt.clientY-ret.y);
 }
 function onMouseUp(evt){
 	pickPoint( getPoint(evt));
@@ -52,9 +27,10 @@ function onMouseUp(evt){
 
 function onMouseMove(evt){
 	if (pickedPoints.length === 0 ){
-		pickedPoints.push(getPoint(evt));
+		pickPoint(getPoint(evt));
+	}else{
+		pickedPoints[pickedPoints.length-1] = getPoint(evt);
 	}
-	pickedPoints[pickedPoints.length-1] = getPoint(evt);
     redraw();
 }
 
@@ -86,3 +62,61 @@ function drawPoint(point){
 	ctx.arc(point.x,point.y,6,0,Math.PI*2,true);
 	ctx.fill();
 }
+
+
+
+function calcSlope(p1,p2){
+	return ((p2.y-p1.y)/(p2.x-p1.x));
+}
+//Points+++++++++++++++++++++++++++++++++++++++++++++++++++++
+	//protótipos
+	
+
+var prot_Point = {
+	x:100,
+	y:100,
+	distanceTo : function(pt){
+		return Math.sqrt(Math.pow(pt.x-thix.x,2)+Math.pow(pt.y-this.y,2));
+	},
+	slopeTo : function(pt){
+		return calcSlope(this,pt);
+	},
+	moveTo: function(x,y){
+		this.x = x;
+		this.y = y;
+	}
+};
+
+var prot_fullPoint = {
+	x:prot_Point.x,
+	y:prot_Point.y,
+	distanceTo:prot_Point.distanceTo,
+	slopeTo:prot_Point.slopeTo,
+	moveTo:prot_Point.moveTo,
+	l: new class_Point(this.x,this.y),
+	r: new class_Point(this.x,this.y),
+	interpolateTo:function (pt){
+		this.r.moveTo(1/3*(pt.x-this.x),1/3*(pt.y-this.y));
+		pt.l.moveTo(2/3*(pt.x-this.x),2/3*(pt.y-this.y));
+	},
+	interpolateBetween:function(pt1,pt2){
+		
+	},
+	interpolateNew:function(pt){
+
+	}
+};
+
+function class_Point(x,y){
+	this.x = x;
+	this.y = y;
+}
+class_Point.prototype = prot_Point;
+
+function class_fullPoint(x,y){
+	this.x = x;
+	this.y = y;
+}
+class_fullPoint.prototype = prot_fullPoint;
+
+//------------------------------------------------------------
