23fcd760ba510926cc32983c8603ebb67af7c477
RogLeite <rodrigo.ldasilva@hotmail.com>
2018-09-12 12:39:24 -0300

    implementa interpolateTo

--- compgraf-t2/bezier.js
+++ compgraf-t2/bezier.js
@@ -1,7 +1,7 @@
 var canvas =document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var pickedPoints = [];
-
+var linear = require("../linear-solve/gauss-jordan.js");
 
 
 
@@ -13,10 +13,11 @@ function pickPoint(point){
 	}
 
 	if(pickedPoints.length===1){
+		alert("pickedPoints[0]  = "+pickedPoints[0]);
 		pickedPoints[0].interpolateTo(point);
-	}/*else if(pickedPoints.length===2){
+	}else if(pickedPoints.length===2){
 		pickedPoints[0].interpolateBetween(pickedPoints[1],point);
-	}else if(pickedPoints.length>2){
+	}/*else if(pickedPoints.length>2){
 		pickedPoints[pickedPoints.length-1].interpolateNew(point);
 	}*/
 	//só para ver se estão sendo desenhados+++
@@ -102,9 +103,8 @@ function drawPoint(point,radius,color){
 
 
 //Points+++++++++++++++++++++++++++++++++++++++++++++++++++++
-	//protótipos
-	
 
+//protótipos
 var prot_Point = {
 	x:100,
 	y:100,
@@ -135,14 +135,49 @@ var prot_fullPoint = {
 	r: new class_Point(this.x,this.y),
 	interpolateTo:function (pt){
 		//alert("interpolateTo");
+
+		/*
+		fonte : http://webserver2.tecgraf.puc-rio.br/~mgattass/cg/2018/03_Bezier.pdf
+		página 26
+		*/
 		this.r.moveBy(1/3*(pt.x-this.x),1/3*(pt.y-this.y));
 		pt.l.moveBy(-1/3*(pt.x-this.x),-1/3*(pt.y-this.y));
 	},
 	interpolateBetween:function(pt1,pt2){
 
+		/*
+		fonte : http://webserver2.tecgraf.puc-rio.br/~mgattass/cg/2018/03_Bezier.pdf
+		página 27
+		*/
+		let p0 = this;
+		let ro = calcRo(p0,p1,p2);
+		let matrix = [
+			[2,-1,0,0],
+			[0,1-ro,ro,0],
+			[1,-2,2,-1],
+			[0,0,-1,2]
+		];
+		let solX,solY;
+		//Resolve componente x
+		solX = linear.solve(matrix,[p0.x,p1.x,0,p2.x]);
+		//solX => [r0.x,l1.x,r1.x,l2.x]
+		
+
+		//resolve componente y
+		solY = linear.solve(matrix,[p0.y,p1.y,0,p2.y]);
+		//solY => [r0.y,l1.y,r1.y,l2.y]
+
+		p0.r.moveTo(solX[0],solY[0]);
+		p1.l.moveTo(solX[1],solY[1]);
+		p1.r.moveTo(solX[2],solY[2]);
+		p2.l.moveTo(solX[3],solY[3]);
+
 	},
 	interpolateNew:function(pt){
-
+		/*
+		fonte : http://webserver2.tecgraf.puc-rio.br/~mgattass/cg/2018/03_Bezier.pdf
+		página 28
+		*/
 	}
 };
 
@@ -160,4 +195,10 @@ function class_fullPoint(x,y){
 }
 class_fullPoint.prototype = prot_fullPoint;
 
+function calcRo(p0,p1,p2){
+	return p0.distanceTo(p1)/sumDistances(p0,p1,p2);
+}
+function sumDistances(p0,p1,p2){
+	return p0.distanceTo(p1)+p1.distanceTo(p2);
+}
 //------------------------------------------------------------
