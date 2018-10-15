41f2cc592b43c0989092817d53ac44021f5cc5c7
RogLeite <rodrigo.ldasilva@hotmail.com>
2018-10-15 11:18:01 -0300

    pouco progresso

--- compgraf-t3/scene.js
+++ compgraf-t3/scene.js
@@ -7,6 +7,8 @@ const STD={
     specular:[1,1,1,1],
     ambient:[0,0,0,0],
     color_sphere:[1,0,0,1],//red
+    color_box:[1,1,0,1],//yellow
+    origin:my_math.matrix([0],[0],[0]),
 };
 
 const prot_Scene = {
@@ -22,12 +24,12 @@ const prot_Scene = {
         solids.forEach(function(element){
             let here = element.checkCollision(P,Origin,max_t);
             if(here){//se houve colisão
-                if (here.dist<obj.dist){
+                if (here.dist<obj.dist){//se está mais proximo da camera
                     obj = here;
                 }
             }
         });
-        return obj.shade(P);
+        return obj.shade(P,obj.dist);
     }
 
 };
@@ -40,6 +42,7 @@ class_Scene.prototype = prot_Scene;
 
 
 const prot_Solid = {
+    origin:STD.origin,
     color_difuse:STD.difuse,
     color_specular:STD.specular,
     color_ambient:STD.ambient,
@@ -47,35 +50,38 @@ const prot_Solid = {
         let t=1;
         return {obj:this,dist:t};
     },
-    shade:function(P){
-        return STD.color;
+    shade:function(P,t){
+        return this.difuse;
     }
 }
 
 const prot_Sphere = {
-    //[[TODO]]
-    center:my_math.matrix([10],[10],[10]),
+    origin:prot_Solid.origin,
     radius:5,
     color_difuse:STD.color_sphere,
-    color_ambient:STD.ambient,
-    color_specular:STD.specular,
+    color_ambient:prot_Solid.ambient,
+    color_specular:prot_Solid.specular,
     checkCollision:function(P,Origin,max_t){
         let local_a = my_math.multiply(P.unit,P.unit);
-        let local_b = my_math.multiply(my_math.multiply(2,P.unit),my_math.add(Origin,my_math.multiply(-1,this.center)));
-        let local_c = my_math.add(my_math.multiply(my_math.add(Origin,my_math.multiply(-1,this.center)),my_math.add(Origin,my_math.multiply(-1,this.center))),-Math.pow(this.radius,2));
+        let local_b = my_math.multiply(my_math.multiply(2,P.unit),my_math.add(Origin,my_math.multiply(-1,this.origin)));
+        let local_c = my_math.add(my_math.multiply(my_math.add(Origin,my_math.multiply(-1,this.origin)),my_math.add(Origin,my_math.multiply(-1,this.origin))),-Math.pow(this.radius,2));
         let delta = Math.pow(local_b,2) - 4*local_a*local_c;
         if(delta<0){
             return false;
         } else if(delta===0){
             let t1 = -local_b/(2*local_a);
-            return {obj:this,t:t1};
+            let l_normal = l_math.add(P(t1),l_math.multiply(-1,this.origin));
+            l_normal = l_math.normalize(l_normal);
+            return {obj:this,t:t1,normal:l_normal};
         }else{
             let t1 = -local_b-Math.sqrt(delta)/(2*local_a);
             let t2 = -local_b+Math.sqrt(delta)/(2*local_a);
-            return {obj:this,t:Math.min(t1,t2)};
+            let l_normal = l_math.add(P(Math.min(t1,t2)),l_math.multiply(-1,this.origin));
+            l_normal = l_math.normalize(l_normal);
+            return {obj:this,t:Math.min(t1,t2),normal:l_normal};
         }
     },
-    shade:function(P){
+    shade:function(P,t){
         //[[TODO]] shader da esfera
         return this.difuse;
 
@@ -85,3 +91,23 @@ function class_Sphere(){
 
 }
 class_Sphere.prototype = prot_Sphere;
+
+
+const prot_Aligned_Box = {
+    origin:prot_Solid.origin,
+    //yu:l_math.matrix([0],[1],[0]),//y unitário
+    //xz_du:l_math.matrix([1],[1],[1]),//diagonal do plano y=0(y do próprio retângulo)
+    width:100,//dx
+    height:5,//dy
+    length:200,//dz
+    color_difuse:STD.color_box,
+    color_ambient:prot_Solid.ambient,
+    color_specular:prot_Solid.specular,
+    checkCollision:function(P,Origin,max_t){
+        //let mod_diagonal = Math.sqrt(Math.pow(this.width,2)+Math.pow(this.length,2));
+        //let diagonal = l_math.multiply(xz_du,mod_diagonal);
+        
+        //[[TODO]] detecção de caixas alinhadas aos eixos
+    },
+    //[[TODO]] shade
+};
