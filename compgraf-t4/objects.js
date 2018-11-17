7fe5d1d50eaf54eb11904a2de2fd1dfc2e1b92d7
RogLeite <rodrigo.ldasilva@hotmail.com>
2018-11-17 10:59:26 -0200

    funcionando

--- compgraf-t4/objects.js
+++ compgraf-t4/objects.js
@@ -1,8 +1,31 @@
-function Cube(origin,side,rotation){
-    //rotation é um quat
-    this.model = this.translation*this.rotation;
-     //Definir vértices e índices
-    this.vertices = [
+
+var prot_Cube = {
+    model:mat4.create(),
+    makeModel:function(translation){
+        mat4.identity(this.model);
+        mat4.translate(this.model,this.model,translation);
+    },
+    draw:function(gl,view,proj){
+        let mvp = mat4.create();
+        mat4.multiply(mvp,view,this.model);
+        mat4.multiply(mvp,proj,mvp);
+        gl.uniformMatrix4fv(program.mvpUniform, false, mvp);
+        //Desenhar
+        gl.drawElements(gl.TRIANGLES, Cube.indices.length, gl.UNSIGNED_SHORT, 0);
+    },
+};
+function Cube(translation){
+    this.makeModel(translation);
+    
+}
+Cube.prototype = prot_Cube;
+
+Cube.bindVertexArray=function(gl){
+
+    gl.bindVertexArray(this.vao);
+
+};
+Cube.vertices = [
     -1.0, -1.0, +1.0,
     +1.0, -1.0, +1.0,
     -1.0, +1.0, +1.0,
@@ -13,7 +36,7 @@ function Cube(origin,side,rotation){
     +1.0, -1.0, -1.0
     ];
 
-    this.indices = [
+Cube.indices = [
     0, 1, 2,
     2, 1, 3,
     2, 3, 4,
@@ -27,7 +50,7 @@ function Cube(origin,side,rotation){
     6, 0, 4,
     4, 0, 2
     ];
-    this.color = [
+Cube.color = [
     1.0, 1.0, 1.0,
     1.0, 1.0, 1.0,
     1.0, 1.0, 1.0,
@@ -37,4 +60,37 @@ function Cube(origin,side,rotation){
     1.0, 0.0, 0.0,
     1.0, 0.0, 0.0
     ];
-}
+Cube.create = function(gl){
+    this.vao = gl.createVertexArray();
+    gl.bindVertexArray(this.vao);
+
+    //Criar VBO dos vertices, linkar e copiar os dados
+    this.vertexVBO = gl.createBuffer();
+    //Define buffer como corrente.
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexVBO);
+    //Aloca buffer e copia dados.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+    //Habilita atributo desejado do vertice.
+    gl.enableVertexAttribArray(program.vertexPosAttr);
+    //Diz que os atributos estao no buffer corrente.
+    gl.vertexAttribPointer(program.vertexPosAttr,3,gl.FLOAT,false,0,0);
+
+    //Criar VBO para cores, linkar e copiar os dados
+    this.vboColor = gl.createBuffer();
+    //Define buffer como corrente.
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vboColor);
+    //Aloca buffer e copia dados.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.color), gl.STATIC_DRAW);
+    //Habilita atributo desejado do vertice.
+    gl.enableVertexAttribArray(program.vertexColorAttr);
+    //Diz que os atributos estao no buffer corrente.
+    gl.vertexAttribPointer(program.vertexColorAttr,3,gl.FLOAT,false,0,0);
+
+    //Criar EBO, linkar e copiar os dados
+    this.EBO = gl.createBuffer();
+    //Define o buffer como corrente e o define como buffer de elementos.
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.EBO);
+    //Aloca buffer e copia dados.
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.indices), gl.STATIC_DRAW);
+
+};
--- compgraf-t4/rendering.js
+++ compgraf-t4/rendering.js
@@ -8,7 +8,9 @@ var gl;
 var program;
 
 //Encapsula um conjunto de definicoes sobre um objeto.
-var vao;
+var vao_Cube;
+
+var simple_cube = new Cube();
 
 //Matrizes de transformacao.
 var view, proj;
@@ -117,81 +119,11 @@ function initScene(){
     mat4.perspective(proj, 45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0);
 
     //Criar o objeto
-    createCube();
+    Cube.create(gl);
 }
 
 
-function createCube(){
-    //Definir vértices e índices
-    var vertices = [
-      -1.0, -1.0, +1.0,
-      +1.0, -1.0, +1.0,
-      -1.0, +1.0, +1.0,
-      +1.0, +1.0, +1.0,
-      -1.0, +1.0, -1.0,
-      +1.0, +1.0, -1.0,
-      -1.0, -1.0, -1.0,
-      +1.0, -1.0, -1.0
-    ];
 
-    var indices = [
-      0, 1, 2,
-      2, 1, 3,
-      2, 3, 4,
-      4, 3, 5,
-      4, 5, 6,
-      6, 5, 7,
-      6, 7, 0,
-      0, 7, 1,
-      1, 7, 3,
-      3, 7, 5,
-      6, 0, 4,
-      4, 0, 2
-    ];
-
-    var color = [
-      1.0, 1.0, 1.0,
-      1.0, 1.0, 1.0,
-      1.0, 1.0, 1.0,
-      1.0, 1.0, 1.0,
-      1.0, 0.0, 0.0,
-      1.0, 0.0, 0.0,
-      1.0, 0.0, 0.0,
-      1.0, 0.0, 0.0
-    ];
-
-    vao = gl.createVertexArray();
-    gl.bindVertexArray(vao);
-
-    //Criar VBO dos vertices, linkar e copiar os dados
-    var vertexVBO = gl.createBuffer();
-	//Define buffer como corrente.
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexVBO);
-	//Aloca buffer e copia dados.
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	//Habilita atributo desejado do vertice.
-    gl.enableVertexAttribArray(program.vertexPosAttr);
-	//Diz que os atributos estao no buffer corrente.
-    gl.vertexAttribPointer(program.vertexPosAttr,3,gl.FLOAT,false,0,0);
-
-    //Criar VBO para cores, linkar e copiar os dados
-    var vboColor = gl.createBuffer();
-	//Define buffer como corrente.
-    gl.bindBuffer(gl.ARRAY_BUFFER, vboColor);
-	//Aloca buffer e copia dados.
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW);
-	//Habilita atributo desejado do vertice.
-    gl.enableVertexAttribArray(program.vertexColorAttr);
-	//Diz que os atributos estao no buffer corrente.
-    gl.vertexAttribPointer(program.vertexColorAttr,3,gl.FLOAT,false,0,0);
-
-    //Criar EBO, linkar e copiar os dados
-    var EBO = gl.createBuffer();
-	//Define o buffer como corrente e o define como buffer de elementos.
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO);
-	//Aloca buffer e copia dados.
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-}
 
 
 
@@ -199,30 +131,19 @@ function redraw(){
     //Definir tamanho e limpar a janela
     gl.viewport(0,0,gl.viewportWidth,gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    gl.bindVertexArray(vao);
-
-    //Desenhar os objetos em diferentes posições
-    var model = mat4.create();
-    var modelView = mat4.create();
-    var mvp = mat4.create();
+    
+    
+    //Desenhar os cubos em diferentes posições
+    Cube.bindVertexArray(gl);
     for( let x = -5; x <= 5; x+=5 )
     {
         for( let z = -5; z <= 5; z+=5)
         {
-            mat4.identity(model);
-            mat4.translate(model,model,[x,0,z]);
-
-            mat4.multiply(modelView,view,model);
-            mat4.multiply(mvp,proj,modelView);
-
-            gl.uniformMatrix4fv(program.mvpUniform, false, mvp);
-
-            //Desenhar
-            gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+            let temp_cube = new Cube([x,0,z]);
+            temp_cube.draw(gl,view,proj);
         }
     }
 
     //Desabilitar buffers habilitados
-      gl.bindVertexArray(null);
+    gl.bindVertexArray(null);
 }
